56d93f841d629a576e20f0bdf07433bc8f1b143f
Thomas Stokes <git@tombl.dev>
2024-08-13 23:16:06 +0800

    simplify/fix atomics
    was hoping this would solve my problem, but still a useful change

--- arch/wasm/include/asm/cmpxchg.h
+++ arch/wasm/include/asm/cmpxchg.h
@@ -4,90 +4,15 @@
 #include <asm/bug.h>
 #include <linux/types.h>
 
-static inline unsigned long __arch_xchg(unsigned long x, volatile void *ptr,
-					int size, int order)
-{
-	switch (size) {
-	case 1:
-		return __atomic_exchange_n((volatile u8 *)ptr, x & 0xffu,
-					   order);
-	case 2:
-		return __atomic_exchange_n((volatile u16 *)ptr, x & 0xffffu,
-					   order);
-	case 4:
-		return __atomic_exchange_n((volatile u32 *)ptr, x & 0xffffffffu,
-					   order);
-#ifdef CONFIG_64BIT
-	case 8:
-		return __atomic_exchange_n((volatile u64 *)ptr, x, order);
-#endif
-	default:
-		BUG();
-	}
-}
-
-#define arch_xchg(ptr, x)                                           \
-	((__typeof__(*(ptr)))__arch_xchg((unsigned long)(x), (ptr), \
-					 sizeof(*(ptr)), __ATOMIC_SEQ_CST))
-#define arch_xchg_relaxed(ptr, x)                                   \
-	((__typeof__(*(ptr)))__arch_xchg((unsigned long)(x), (ptr), \
-					 sizeof(*(ptr)), __ATOMIC_RELAXED))
-#define arch_xchg_acquire(ptr, x)                                   \
-	((__typeof__(*(ptr)))__arch_xchg((unsigned long)(x), (ptr), \
-					 sizeof(*(ptr)), __ATOMIC_ACQUIRE))
-#define arch_xchg_release(ptr, x)                                   \
-	((__typeof__(*(ptr)))__arch_xchg((unsigned long)(x), (ptr), \
-					 sizeof(*(ptr)), __ATOMIC_RELEASE))
-
-static inline unsigned long __arch_cmpxchg(volatile void *ptr,
-					   unsigned long old, unsigned long new,
-					   int size, int order)
-{
-	switch (size) {
-	case 1:
-		return __atomic_compare_exchange_n((volatile u8 *)ptr,
-						   (u8 *)&old, new & 0xffu, 0,
-						   order, __ATOMIC_SEQ_CST) ?
-			       old :
-			       new;
-	case 2:
-		return __atomic_compare_exchange_n((volatile u16 *)ptr,
-						   (u16 *)&old, new & 0xffffu,
-						   0, order, __ATOMIC_SEQ_CST) ?
-			       old :
-			       new;
-	case 4:
-		return __atomic_compare_exchange_n((volatile u32 *)ptr,
-						   (u32 *)&old,
-						   new & 0xffffffffu, 0, order,
-						   __ATOMIC_SEQ_CST) ?
-			       old :
-			       new;
-#ifdef CONFIG_64BIT
-	case 8:
-		return __atomic_compare_exchange_n((volatile u64 *)ptr,
-						   (u64 *)&old, new, 0, order,
-						   __ATOMIC_SEQ_CST)
-#endif
-			default : BUG();
-	}
-}
+#define arch_xchg(ptr, x) __atomic_exchange_n((ptr), (x), __ATOMIC_SEQ_CST)
 
 #define arch_cmpxchg(ptr, old, new)                                      \
-	((__typeof__(*(ptr)))__arch_cmpxchg((ptr), (unsigned long)(old), \
-					    (unsigned long)(new),        \
-					    sizeof(*(ptr)), __ATOMIC_SEQ_CST))
-#define arch_cmpxchg_relaxed(ptr, old, new)                              \
-	((__typeof__(*(ptr)))__arch_cmpxchg((ptr), (unsigned long)(old), \
-					    (unsigned long)(new),        \
-					    sizeof(*(ptr)), __ATOMIC_RELAXED))
-#define arch_cmpxchg_acquire(ptr, old, new)                              \
-	((__typeof__(*(ptr)))__arch_cmpxchg((ptr), (unsigned long)(old), \
-					    (unsigned long)(new),        \
-					    sizeof(*(ptr)), __ATOMIC_ACQUIRE))
-#define arch_cmpxchg_release(ptr, old, new)                              \
-	((__typeof__(*(ptr)))__arch_cmpxchg((ptr), (unsigned long)(old), \
-					    (unsigned long)(new),        \
-					    sizeof(*(ptr)), __ATOMIC_RELEASE))
+	({                                                               \
+		typeof(*(ptr)) __old = (old), __new = (new);             \
+		__atomic_compare_exchange_n((ptr), &__old, __new, false, \
+					    __ATOMIC_SEQ_CST,            \
+					    __ATOMIC_RELAXED);           \
+		__old;                                                   \
+	})
 
 #endif
