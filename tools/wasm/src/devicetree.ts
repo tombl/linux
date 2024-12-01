ad47e85e4607b11087aa56a8f9b46a83588ca715
Thomas Stokes <git@tombl.dev>
2024-12-02 00:44:49 +0800

    add structs to javascript, migrate devicetree to them

--- tools/wasm/src/devicetree.ts
+++ tools/wasm/src/devicetree.ts
@@ -1,3 +1,12 @@
+import {
+  type Allocated,
+  Bytes,
+  Struct,
+  U32BE,
+  U64BE,
+  U8,
+  type Unwrap,
+} from "./bytes.ts";
 import { assert, unreachable } from "./util.ts";
 
 const FDT_MAGIC = 0xd00dfeed;
@@ -23,65 +32,63 @@ type DeviceTreeProperty =
   | ArrayBuffer
   | undefined;
 
-function inner(
-  tree: DeviceTreeNode,
-  memory_reservations: Array<
-    { address: number | bigint; size: number | bigint }
-  >,
-  boot_cpu_id: number,
-  max_size: number,
-): Uint8Array {
-  const arr = new Uint8Array(max_size);
-  const dv = new DataView(arr.buffer);
-  let i = 0;
+// https://devicetree-specification.readthedocs.io/en/latest/chapter5-flattened-format.html
+const FdtHeader = Struct({
+  magic: U32BE,
+  totalsize: U32BE,
+  off_dt_struct: U32BE,
+  off_dt_strings: U32BE,
+  off_mem_rsvmap: U32BE,
+  version: U32BE,
+  last_comp_version: U32BE,
+  boot_cpuid_phys: U32BE,
+  size_dt_strings: U32BE,
+  size_dt_struct: U32BE,
+});
 
-  function u8(n = 0x78) {
-    const j = i;
-    dv.setUint8(j, n);
-    i += 1;
-    return j;
+const FdtReserveEntry = Struct({
+  address: U64BE,
+  size: U64BE,
+});
+
+const Property = Struct({
+  len: U32BE,
+  nameoff: U32BE,
+});
+type Property = Unwrap<typeof Property>;
+
+function align(bytes: Bytes, alignment: number) {
+  const offset = bytes.length % alignment;
+  if (offset !== 0) {
+    for (let j = 0; j < alignment - offset; j++) bytes.alloc(U8);
   }
-  function u32(n = 0x78787878) {
-    const j = i;
-    dv.setUint32(j, n);
-    i += 4;
-    return j;
-  }
-  function u64(n: number | bigint = 0x7878787878787878n) {
-    const j = i;
-    dv.setBigUint64(j, BigInt(n));
-    i += 8;
-    return j;
-  }
-  function bytes(buf: ArrayBuffer) {
-    const j = i;
-    arr.set(new Uint8Array(buf), i);
-    i += buf.byteLength;
-    return j;
-  }
-  function stringz(s: string) {
-    const j = bytes(new TextEncoder().encode(s).buffer);
-    u8(0);
-    return j;
-  }
-  function pad(align = 4) {
-    const offset = i % align;
-    if (offset !== 0) {
-      for (let j = 0; j < align - offset; j++) u8(0);
-    }
-  }
+}
+
+export function generate_devicetree(tree: DeviceTreeNode, {
+  memory_reservations = [],
+  boot_cpu_id = 0,
+}: {
+  memory_reservations?: Array<
+    { address: number; size: number }
+  >;
+  boot_cpu_id?: number;
+} = {}) {
+  const bytes = new Bytes(1024);
+  const strings: Record<string, Allocated<Property>[]> = {};
+  const header = bytes.alloc(FdtHeader);
 
   function walk_tree(node: DeviceTreeNode, name: string) {
-    const startOffset = i;
-    pad();
-    u32(FDT_BEGIN_NODE);
+    align(bytes, 4);
+    bytes.alloc(U32BE).value = FDT_BEGIN_NODE;
 
+    const encodedName = new TextEncoder().encode(name);
     assert(
-      new TextEncoder().encode(name).byteLength <= NODE_NAME_MAX_LEN,
+      encodedName.byteLength <= NODE_NAME_MAX_LEN,
       `property name too long: ${name}`,
     );
-    stringz(name);
-    pad();
+    bytes.append(encodedName);
+    bytes.alloc(U8).value = 0;
+    align(bytes, 4);
 
     const children = Object.entries(node).filter(
       ([, value]) => typeof value === "object" && value?.constructor === Object,
@@ -93,15 +100,15 @@ function inner(
     ) as [string, DeviceTreeProperty][];
 
     for (const [name, prop] of properties) {
-      pad();
-      u32(FDT_PROP);
-      const len = u32();
+      align(bytes, 4);
+      bytes.alloc(U32BE).value = FDT_PROP;
+      const property = bytes.alloc(Property);
 
       assert(
         new TextEncoder().encode(name).byteLength <= PROPERTY_NAME_MAX_LEN,
         `property name too long: ${name}`,
       );
-      (strings[name] ??= []).push(u32());
+      (strings[name] ??= []).push(property);
 
       let value: ArrayBuffer;
       switch (typeof prop) {
@@ -140,71 +147,50 @@ function inner(
         default:
           unreachable(prop, `unsupported prop type: ${typeof prop}`);
       }
-      dv.setUint32(len, value.byteLength);
-      bytes(value);
-      pad();
+      property.value.len = value.byteLength;
+      bytes.append(new Uint8Array(value));
+      align(bytes, 4);
     }
     for (const [name, child] of children) walk_tree(child, name);
 
-    pad();
-    u32(FDT_END_NODE);
-    return i - startOffset;
+    align(bytes, 4);
+    bytes.alloc(U32BE).value = FDT_END_NODE;
   }
 
-  const strings: Record<string, number[]> = {};
-  try {
-    u32(FDT_MAGIC); // magic
-    const totalsize = u32();
-    const off_dt_struct = u32();
-    const off_dt_strings = u32();
-    const off_mem_rsvmap = u32();
-    u32(17); // version
-    u32(16); // last compatible version
-    u32(boot_cpu_id); // boot cpuid phys
-    const size_dt_strings = u32();
-    const size_dt_struct = u32();
+  Object.assign(header.value, {
+    magic: FDT_MAGIC,
+    version: 17,
+    last_comp_version: 16,
+    boot_cpuid_phys: boot_cpu_id,
+  });
 
-    pad(8);
-    dv.setUint32(off_mem_rsvmap, i);
-    for (const { address, size } of memory_reservations) {
-      u64(address);
-      u64(size);
-    }
-    u64(0);
-    u64(0);
+  align(bytes, 8);
 
-    const begin_dt_struct = i;
-    dv.setUint32(off_dt_struct, begin_dt_struct);
-    walk_tree(tree, "");
-    u32(FDT_END);
-    dv.setUint32(size_dt_struct, i - begin_dt_struct);
-
-    const begin_dt_strings = i;
-    dv.setUint32(off_dt_strings, begin_dt_strings);
-    for (const [str, refs] of Object.entries(strings)) {
-      const offset = stringz(str);
-      for (const ref of refs) dv.setUint32(ref, offset - begin_dt_strings);
-    }
-    dv.setUint32(size_dt_strings, i - begin_dt_strings);
-
-    dv.setUint32(totalsize, i);
-    return arr.slice(0, i);
-  } catch (err) {
-    if (err instanceof RangeError) {
-      return inner(tree, memory_reservations, boot_cpu_id, max_size * 2);
-    }
-    throw err;
+  header.value.off_mem_rsvmap = bytes.length;
+  for (const { address, size } of memory_reservations) {
+    bytes.alloc(FdtReserveEntry).value = {
+      address: BigInt(address),
+      size: BigInt(size),
+    };
   }
-}
+  bytes.alloc(FdtReserveEntry).value = { address: 0n, size: 0n };
 
-export function generate_devicetree(tree: DeviceTreeNode, {
-  memory_reservations = [],
-  boot_cpu_id = 0,
-}: {
-  memory_reservations?: Array<
-    { address: number; size: number }
-  >;
-  boot_cpu_id?: number;
-} = {}) {
-  return inner(tree, memory_reservations, boot_cpu_id, 1024);
+  const begin_dt_struct = bytes.length;
+  header.value.off_dt_struct = begin_dt_struct;
+  walk_tree(tree, "");
+  bytes.alloc(U32BE).value = FDT_END;
+  header.value.size_dt_struct = bytes.length - begin_dt_struct;
+
+  const begin_dt_strings = bytes.length;
+  header.value.off_dt_strings = begin_dt_strings;
+  for (const [str, refs] of Object.entries(strings)) {
+    const offset = bytes.length;
+    bytes.append(new TextEncoder().encode(str));
+    bytes.alloc(U8).value = 0;
+    for (const ref of refs) ref.value.nameoff = offset - begin_dt_strings;
+  }
+  header.value.size_dt_strings = bytes.length - begin_dt_strings;
+
+  header.value.totalsize = bytes.length;
+  return bytes.array;
 }
